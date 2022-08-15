28b629915b01eec3a70dfd8fbd53832b46f37781
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-15 12:59:24 -0700

    Created post route for adding new Tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,10 +49,19 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
-  /* req.body should look like this
+  /* req.body should look like this:
+    {
+      tag_name: "some name"
+    }
   */
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
