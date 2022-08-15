ee0db4b4c5ef7c4b6404a70e1bf1326651a4ea2e
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-15 11:35:17 -0700

    Added error message for delete operation

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -147,7 +147,7 @@ router.delete('/:id', async (req, res) => {
       res.status(200).json(productData);
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "No product with that ID!" });
   }
 });
 
