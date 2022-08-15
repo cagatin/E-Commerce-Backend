8b42ac0ab36a9496d4bc8b6cb30f1f214906f118
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-15 12:03:41 -0700

    Created get route for specific tag ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -25,9 +25,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(
+      req.params.id,
+      {
+        include: [
+          {
+            model: Product,
+          }
+        ]
+      }
+    );
+    if (!tagData) {
+      res.status(404).json({ message: `No Tag found with the ID of ${res.params.id}` });
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(404).json({ message: `No Tag found with the ID of ${res.params.id}` });
+  }
 });
 
 router.post('/', (req, res) => {
