49e4bfc4951b51ac27a76128ccc7fc7577cff2b8
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-15 13:20:50 -0700

    fixed put route and created delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,12 +64,46 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    // retrieve the tag with the requested ID
+    const tagData = await Tag.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!tagData[0]) {
+      res.status(404).json({ message: "No tag with the specified ID found!" });
+      return;
+    } else {
+      res.status(200).json({ message: "Successfully updated!" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "No Tag with the specified ID found!" });
+      return;
+    } else {
+      res.status(200).json(tagData);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
