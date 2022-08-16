351a2e9ae7e74d05ca9c704d6432ef0c42fae3b7
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-16 10:44:56 -0700

    Created update route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { update } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
@@ -46,7 +47,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   /* req.body should look like this:
     {
@@ -77,8 +78,38 @@ router.post('/', (req, res) => {
 
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    //Check if the ID exists in the table
+    const categoryData = await Category.findByPk(
+      req.params.id,
+      {
+        include: [
+          {
+            model: Product
+          }
+        ]
+      }
+    );
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found that matches ID." });
+      return;
+    }
+
+    // Update the row
+    const updateData = await Category.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json({ message: "Sucessfully Updated!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
